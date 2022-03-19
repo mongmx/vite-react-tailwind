9d5975b0c80f725099e1bf8011970b075437a501
Mong MX <mongmx@gmail.com>
2022-03-20 06:01:50 +0700

    Add avatar to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <header className="sticky top-0 bg-white text-white shadow p-4 uppercase">
-      Page name
+      <div class="inline-flex items-center justify-center w-12 h-12 text-xl text-white bg-blue-900 rounded-full right-0">
+        TW
+      </div>
     </header>
   );
 }
