f7969ab0c01ca4d39118d1d1578f7dc41df23ab6
Mong MX <mongmx@gmail.com>
2022-03-21 06:42:28 +0700

    Add localStorage example

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
 import PageTitle from "../components/PageTitle";
 
 const Dashboard = () => {
+  const [name, setName] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("name");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("name", JSON.stringify(name));
+  }, [name]);
+
   return (
     <>
       <PageTitle>Dashboard</PageTitle>
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full name"
+          aria-label="fullname"
+        />
+        <input type="submit" value="Submit"></input>
+      </form>
     </>
   );
 }
