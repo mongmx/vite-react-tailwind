1b02b652b615f627159b00f3d8cf8a86cc960f6c
MongMX <mongmx@gmail.com>
2022-03-29 02:49:36 +0700

    Add submit handler

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import PageTitle from "../components/PageTitle";
+import Button from "../components/Button";
 
 const Dashboard = () => {
   const [name, setName] = useState(() => {
@@ -14,19 +15,27 @@ const Dashboard = () => {
     localStorage.setItem("name", JSON.stringify(name));
   }, [name]);
 
+  const handleSubmit = (e) => {
+    alert('A name was submitted: ' + name);
+    e.preventDefault();
+  };
+
   return (
     <>
       <PageTitle>Dashboard</PageTitle>
-      <form>
+      <div className="w-64 mt-4 grid grid-cols-1 gap-6">
+        <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Full name"
           aria-label="fullname"
+          className="w-full mt-1 block rounded-md border-gray-300 shadow-sm focus:border-blue-500"
         />
-        <input type="submit" value="Submit"></input>
+        <Button type="submit" value="Submit">Submit</Button>
       </form>
+      </div>
     </>
   );
 }
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,8 +1,8 @@
 export default function Welcome() {
   return (
     <div 
-      className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center" 
-      style={{backgroundImage: "url('https://images.unsplash.com/photo-1514897575457-c4db467cf78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920')"}}>
+      className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center bg-blue-900">
+      {/* style={{backgroundImage: "url('https://images.unsplash.com/photo-1514897575457-c4db467cf78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920')"}} */}
       <div className="h-full flex items-center justify-center">
         <div className="w-auto p-4 bg-white shadow-lg my-16 text-gray-600">
           <div className="mb-10 text-center text-4xl">Welcome to BRANDNAME</div>
